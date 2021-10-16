807842b33d17679d86ea66c0e1bec04ac0a7b143
sonalisharan <angelsona1627@gmail.com>
2021-10-16 11:12:59 +0530

    restaurant

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import HomeLayoutHOC from "./HOC/Home.HOC";
 
 //Component
 import Temp from "./Components/temp";
+import Master from "./Components/master";
 
 function App() {
   return (
     <>
-      <HomeLayoutHOC  path="/" exact component={Temp} />
-    </>
+    <HomeLayoutHOC path="/" exact component={Temp}/>
+    <HomeLayoutHOC path="/:type" exact component={Master}/>
+    <RestaurantLayout path="restaurant/:id" exact component ={xyz} />
+     </>
   )
 }
 
 export default App;
-
--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -1,17 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom"; 
 
+import { useDispatch } from "react-router";
 //Components
-import Navbar  from "../Components/Navbar";
+import Navbar from "../Components/Navbar";
 import FoodTab from "../Components/FoodTab";
 
+//redux actions
+import { getRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
+
 const HomeLayout = (props) => {
-    return <> 
-    <div className="container mx-auto lg:px-20">
-        <Navbar />
-     {props.children}
-    </div>
-    <FoodTab/>
- </>
+const dispatch = useDispatch();
+
+useEffect(()=> {
+  dispatch(getRestaurant());
+}, []);
+
+  return <>
+   <div className="container mx-auto lg:px-20">
+   <Navbar />
+
+    {props.children}
+   </div>
+   <FoodTab />
+  </>
 };
 
 export default HomeLayout;
